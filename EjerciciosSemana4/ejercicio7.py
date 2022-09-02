4b667988dfe62ff919a38a92d477da9c5db7a908
JorgeQMTZ <98997614+JorgeQMTZ@users.noreply.github.com>
2022-09-02 15:41:16 -0500

    Add files via upload

---
+++ EjerciciosSemana4/ejercicio7.py
@@ -0,0 +1,14 @@
+'''
+Demostración de implementación de pilas utilizando deques
+'''
+import collections
+SEPARADOR = ("*" * 20) + "\n"
+
+pila_deque = collections.deque()
+for i in range(5):
+    pila_deque.append(input("Dime el nombre a agregar: "))
+
+
+while pila_deque:
+    print(pila_deque.pop())
+pass
---
+++ EjerciciosSemana4/ejercicio9.py
@@ -0,0 +1,15 @@
+letters = []
+
+letters.append('c')
+letters.append('a')
+letters.append('t')
+letters.append('g')
+
+last_item = letters.pop()
+print(last_item)
+
+
+last_item = letters.pop()
+print(last_item)
+
+print(letters)
